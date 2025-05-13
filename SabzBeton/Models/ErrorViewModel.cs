78582123a9b854080a17a6a74a128c96093a6523
MohamadAminA <a.aghakabiri@gmail.com>
2025-05-14 01:13:02 +0330

    Add project files.

---
+++ SabzBeton/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SabzBeton.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
